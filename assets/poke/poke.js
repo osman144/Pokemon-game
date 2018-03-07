159c16405f06bcc4fc806d8dc59df3695b9bb9fd
osman144 <osman144@umn.edu>
2018-03-07 13:04:55 -0600

    initial changes

---
+++ assets/poke/poke.js
@@ -0,0 +1,35 @@
+"use strict"
+
+//The purpose of this game is to randomize pokemon and then generate a choice. The user should guess the 
+//of the pokemon 
+//pokemon choices available 
+let pokeMon = ["pikachu", "squirtle" ];
+
+
+
+
+
+
+//Number of wins user will have 
+
+let userWins = 0;
+
+//Number of losses user will have 
+let userLosses = 0;
+
+//Number of guesses given for game
+
+let userGuesses = 15;
+
+//Here, it should log every choice used for the pokemon/word 
+
+let userPokemonSelection = [];
+
+let compChoice = Math.floor(Math.random()*pokeMon.length);
+
+$(document).ready(function{
+
+})
+
+
+
